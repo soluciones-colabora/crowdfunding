9997d12660f787552473997d890faf8652891f60
José Alejandro Castillo Pinkus <jose.castillo@springlabs.net>
2018-11-11 13:09:28 -0600

    Added auth service

---
+++ src/app/services/authentication/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private afAuth: AngularFireAuth) { }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+}
