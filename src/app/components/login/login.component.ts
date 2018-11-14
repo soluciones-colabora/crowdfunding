e0b1caa9b1183dd8fb758481e02a2405412c412d
José Alejandro Castillo Pinkus <jose.castillo@springlabs.net>
2018-11-14 16:35:57 -0600

    Added facebook and google login, structured card for email login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/authentication/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
+  }
+
+  getErrorPassword() {
+    return this.password.hasError('required') ? 'You must enter a value' :
+            '';
+  }
+
+
+  constructor(public auth: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
+
+  }
+
+  async signInWithFacebook() {
+    console.log('I was clicked!');
+    await this.auth.facebookLogin();
+    await this.afterSignIn();
+  }
+
+  /// Shared
+
+  private afterSignIn() {
+    // Do after login stuff here, such router redirects, toast messages, etc.
+
+    return this.router.navigate(['/']);
   }
 
 }
