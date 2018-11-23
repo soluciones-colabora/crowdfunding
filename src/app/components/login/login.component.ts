daee7e9de9093270c129d44b4fc839cd97b8b506
José Alejandro Castillo Pinkus <jose.castillo@springlabs.net>
2018-11-23 13:51:44 -0600

    Implemented tabs inside the card for email registration

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/authentication/auth.service';
+
+import { EmailAvailableValidator } from '../../validators/email-available.directive';
+import { matchPasswordValidator } from '../../validators/match-password.directive';
 
 @Component({
   selector: 'app-login',
@@ -10,26 +13,53 @@ import { AuthService } from '../../services/authentication/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required]);
+  public formulario: FormGroup;
+  // email = new FormControl('', {
+  //   updateOn: 'blur',
+  //   validators: Validators.compose([Validators.required, Validators.email]),
+  //   asyncValidators : this.emailAvailable.validate.bind(this.emailAvailable)
+  // });
+  // password = new FormControl('', [Validators.required]);
 
-  getErrorMessage() {
-    return this.email.hasError('required') ? 'You must enter a value' :
-        this.email.hasError('email') ? 'Not a valid email' :
+  getErrorMessage( fieldName: string) {
+    // console.log('this.formulario.get(fieldName) :', this.formulario.get(fieldName).errors);
+    return this.formulario.get(fieldName).hasError('required') ? 'You must enter a value' :
+            this.formulario.get(fieldName).hasError('email') ? 'Not a valid email' :
+            this.formulario.get(fieldName).hasError('emailAvailable') ? 'This email has already been registered' :
             '';
   }
 
-  getErrorPassword() {
-    return this.password.hasError('required') ? 'You must enter a value' :
+  getErrorForm() {
+    return this.formulario.hasError('matchPassword') ? 'The password fields must have the same value' :
             '';
   }
 
 
   constructor(public auth: AuthService,
-              private router: Router) { }
+              private router: Router,
+              private emailAvailable: EmailAvailableValidator,
+              private formBuilder: FormBuilder) {
+
+                this.formulario = this.formBuilder.group({
+                  // Datos de usuario
+                  email: ['', {
+                    updateOn: 'blur',
+                    validators: Validators.compose([Validators.required, Validators.email]),
+                    asyncValidators : this.emailAvailable.validate.bind(this.emailAvailable)
+                  }],
+                  password: ['', Validators.compose([Validators.required, Validators.pattern(/^[a-zA-Z0-9_-]{6,18}/),
+                    ])],
+                  password_confirm: ['', Validators.required]
+                }, { validator: Validators.compose([matchPasswordValidator]) });
+  }
+
 
   ngOnInit() {
 
+  }
+
+  register() {
+    console.log('Te estás registrando');
   }
 
   async signInWithFacebook() {
