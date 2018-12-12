d8ec005ff78f6aa31e1d19b66711411164d0f325
José Alejandro Castillo Pinkus <jose.castillo@springlabs.net>
2018-12-12 17:27:47 -0600

    Login page updated to look and work like the given design

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/authentication/auth.service';
 
-import { EmailAvailableValidator } from '../../validators/email-available.directive';
+import { EmailRegisteredValidator } from '../../validators/email-registered.directive';
 import { matchPasswordValidator } from '../../validators/match-password.directive';
 
 @Component({
@@ -14,30 +14,33 @@ import { matchPasswordValidator } from '../../validators/match-password.directiv
 export class LoginComponent implements OnInit {
 
   public formulario: FormGroup;
+  public hide = true;
   // email = new FormControl('', {
   //   updateOn: 'blur',
   //   validators: Validators.compose([Validators.required, Validators.email]),
-  //   asyncValidators : this.emailAvailable.validate.bind(this.emailAvailable)
+  //   asyncValidators : this.emailRegistered.validate.bind(this.emailRegistered)
   // });
   // password = new FormControl('', [Validators.required]);
 
-  getErrorMessage( fieldName: string) {
-    // console.log('this.formulario.get(fieldName) :', this.formulario.get(fieldName).errors);
-    return this.formulario.get(fieldName).hasError('required') ? 'You must enter a value' :
-            this.formulario.get(fieldName).hasError('email') ? 'Not a valid email' :
-            this.formulario.get(fieldName).hasError('emailAvailable') ? 'This email has already been registered' :
-            '';
+  getErrorEmail ( ) {
+    return  this.formulario.get('email').hasError('required') ? 'Ingrese su correo' :
+            this.formulario.get('email').hasError('email') ? 'Correo electrónico inválido' :
+            this.formulario.get('email').hasError('emailRegistered') ? 'El correo electrónico no ha sido registrado' : '';
   }
 
-  getErrorForm() {
-    return this.formulario.hasError('matchPassword') ? 'The password fields must have the same value' :
-            '';
+  getErrorPassword ( ) {
+    return this.formulario.get('password').hasError('required') ? 'Ingrese su contraseña' : '';
   }
+
+  // getErrorForm() {
+  //   return this.formulario.hasError('matchPassword') ? 'The password fields must have the same value' :
+  //           '';
+  // }
 
 
   constructor(public auth: AuthService,
               private router: Router,
-              private emailAvailable: EmailAvailableValidator,
+              private emailRegistered: EmailRegisteredValidator,
               private formBuilder: FormBuilder) {
 
                 this.formulario = this.formBuilder.group({
@@ -45,12 +48,11 @@ export class LoginComponent implements OnInit {
                   email: ['', {
                     updateOn: 'blur',
                     validators: Validators.compose([Validators.required, Validators.email]),
-                    asyncValidators : this.emailAvailable.validate.bind(this.emailAvailable)
+                    asyncValidators : this.emailRegistered.validate.bind(this.emailRegistered)
                   }],
-                  password: ['', Validators.compose([Validators.required, Validators.pattern(/^[a-zA-Z0-9_-]{6,18}/),
-                    ])],
-                  password_confirm: ['', Validators.required]
-                }, { validator: Validators.compose([matchPasswordValidator]) });
+                  password: ['', Validators.required],
+                  rememberPassword: [false]
+                });
   }
 
 
@@ -58,8 +60,19 @@ export class LoginComponent implements OnInit {
 
   }
 
-  register() {
+  getPassword() {
+    console.log('Olvidé contraseña');
+  }
+
+  async register() {
     console.log('Te estás registrando');
+    const email = this.formulario.get('email').value;
+    const password = this.formulario.get('password').value;
+    // Expiration time should vary depending on this value:
+    const rememberPassword = this.formulario.get('rememberPassword').value;
+
+    await this.auth.emailLogin(email, password);
+    await this.afterSignIn();
   }
 
   async signInWithFacebook() {
